afa5a82f37b96e37370c6bb3db72ceebfa3a7068
Kunji Meena <admin@Admins-MBP.Dlink>
2021-12-26 01:08:27 +0530

    .

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import Web3 from 'web3';
+const web3 = new Web3("wss://mainnet.infura.io/ws/v3/accaef31187947eea261471f2e8c9b1b");
 
 type Data = {
   name: string
@@ -9,5 +11,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  console.log(web3.eth.accounts);
   res.status(200).json({ name: 'John Doe' })
 }
